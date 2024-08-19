4cdc0f6104f7a685b33a3200398c97f68ac34c02
Ryan <155923805+ryanbrodey@users.noreply.github.com>
2024-08-18 18:15:21 -0700

    Update reddit_bot.py

--- Reddit-Bot/reddit_bot.py
+++ Reddit-Bot/reddit_bot.py
@@ -1,10 +1,10 @@
 import praw
 
-# This section connects to the Reddit API using Python Code
+# This code below connects to the Reddit API using Python Code
 username="Responsible-Draw-99"
-password="tokoyami8"
-client_id="6DIPzFIUKu3OZF4J6jdxtw"
-client_secret="ucuYqaeO10yeU8AkmQSpmMGfTIu6pg"
+password="YOUR_PASSWORD"
+client_id="YOUR_ID"
+client_secret="YOUR_SECRET_ID"
 
 reddit_instance = praw.Reddit(
     client_id=client_id,
@@ -16,16 +16,19 @@ reddit_instance = praw.Reddit(
 
 # print(reddit_instance.user.me())
 
+# Section 1
 # This section retrieves submissions from subreddits
 # subreddit = reddit_instance.subreddit("Nintendo")
 # top_25_submissions = subreddit.top(limit=25, time_filter="week")
 # for submission in top_25_submissions:
 #    print(submission.title)
 
+# Section 2
 # This section submits post in any subreddit you choose with any message 
 subreddit = reddit_instance.subreddit("testingground4bots")
 subreddit.submit(title="This is my test post from my bot", selftext="Hellow how are you doing")
 
+# Section 3
 # This section gets comments from a subreddit post, and has a part that is also capable of replying to comments 
 # submission = reddit_instance.submission("1ev919g")
 # comments = submission.comments
